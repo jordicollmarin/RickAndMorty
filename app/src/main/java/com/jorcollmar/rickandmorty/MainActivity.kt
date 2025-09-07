2684a69262dac8fd8748a32b3d63eb70a8e270ef
Jordi Coll Marin <jordi.collmarin@justeattakeaway.com>
2025-09-07 18:58:14 +0200

    core: project init.

---
+++ app/src/main/java/com/jorcollmar/rickandmorty/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.jorcollmar.rickandmorty
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jorcollmar.rickandmorty.ui.theme.RickAndMortyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RickAndMortyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RickAndMortyTheme {
+
+    }
+}
