f95d3da41d907b45f94b9d79d0acc9cdcfc862af
Jordi Coll Marin <jordi.collmarin@justeattakeaway.com>
2025-09-08 22:40:52 +0200

    feat: Create AppModule to provide the Retrofit, RickAndMortyDatabase and Pager instances.

---
+++ app/src/main/java/com/jorcollmar/rickandmorty/di/AppModule.kt
@@ -0,0 +1,64 @@
+package com.jorcollmar.rickandmorty.di
+
+import android.content.Context
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.room.Room
+import com.jorcollmar.rickandmorty.data.local.RickAndMortyDatabase
+import com.jorcollmar.rickandmorty.data.local.episodes.EpisodeEntity
+import com.jorcollmar.rickandmorty.data.remote.RickAndMortyApi
+import com.jorcollmar.rickandmorty.data.remote.RickAndMortyRemoteMediator
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+@OptIn(ExperimentalPagingApi::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): RickAndMortyApi {
+        return Retrofit
+            .Builder()
+            .baseUrl(RickAndMortyApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RickAndMortyApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideBeerDatabase(@ApplicationContext context: Context): RickAndMortyDatabase {
+        return Room.databaseBuilder(
+            context,
+            RickAndMortyDatabase::class.java,
+            "rickandmorty.db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRickAndMortyPager(
+        rickAndMortyDatabase: RickAndMortyDatabase,
+        rickAndMortyApi: RickAndMortyApi
+    ): Pager<Int, EpisodeEntity> {
+        return Pager(
+            config = androidx.paging.PagingConfig(pageSize = 20),
+            remoteMediator = RickAndMortyRemoteMediator(
+                rickAndMortyApi,
+                rickAndMortyDatabase
+            ),
+            pagingSourceFactory = {
+                rickAndMortyDatabase.getEpisodeDao().pagingSource()
+            }
+        )
+
+    }
+}
