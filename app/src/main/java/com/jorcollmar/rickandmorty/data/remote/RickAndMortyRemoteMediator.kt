84b28ae5fe18bc7c80bbe685dea3ad0652ec100e
Jordi Coll Marin <jordi.collmarin@justeattakeaway.com>
2025-09-08 22:39:48 +0200

    feat: create RemoteMediator to handle paging and persistence logic.

---
+++ app/src/main/java/com/jorcollmar/rickandmorty/data/remote/RickAndMortyRemoteMediator.kt
@@ -0,0 +1,60 @@
+package com.jorcollmar.rickandmorty.data.remote
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
+import coil.network.HttpException
+import com.jorcollmar.rickandmorty.data.local.RickAndMortyDatabase
+import com.jorcollmar.rickandmorty.data.local.episodes.EpisodeEntity
+import com.jorcollmar.rickandmorty.data.mapper.toEpisodeEntity
+import com.jorcollmar.rickandmorty.data.mapper.toPagingEntity
+
+const val PAGING_KEY = "paging_key"
+private const val FIRST_PAGE = 1
+
+@OptIn(ExperimentalPagingApi::class)
+class RickAndMortyRemoteMediator(
+    private val rickAndMortyApi: RickAndMortyApi,
+    private val rickAndMortyDatabase: RickAndMortyDatabase
+) : RemoteMediator<Int, EpisodeEntity>() {
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, EpisodeEntity>
+    ): MediatorResult {
+        return try {
+            val page = when (loadType) {
+                LoadType.REFRESH -> FIRST_PAGE
+                LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
+                LoadType.APPEND -> {
+                    rickAndMortyDatabase.getPagingDao().getNext(PAGING_KEY)
+                }
+            }
+
+            val response = rickAndMortyApi.getEpisodes(page)
+            val episodes = response.results
+            val pagingDto = response.info
+
+            rickAndMortyDatabase.withTransaction {
+                if (loadType == LoadType.REFRESH) {
+                    rickAndMortyDatabase.getEpisodeDao().clearCache()
+                    rickAndMortyDatabase.getPagingDao().clearPaging()
+                }
+
+                rickAndMortyDatabase.getEpisodeDao()
+                    .addEpisodes(episodes.map {
+                        it.toEpisodeEntity()
+                    })
+
+                rickAndMortyDatabase.getPagingDao().addPaging(pagingDto.toPagingEntity())
+            }
+
+            MediatorResult.Success(endOfPaginationReached = pagingDto.next == null)
+        } catch (exception: HttpException) {
+            MediatorResult.Error(exception)
+        } catch (exception: Exception) {
+            MediatorResult.Error(exception)
+        }
+    }
+}
