2def61295972bbe9ddbdbee8a49ccd5399bf6030
Jordi Coll Marin <jordi.collmarin@justeattakeaway.com>
2025-09-17 23:40:57 +0200

    fix: add error handling in the repository.

--- app/src/main/java/com/jorcollmar/rickandmorty/domain/CharacterRepository.kt
+++ app/src/main/java/com/jorcollmar/rickandmorty/domain/CharacterRepository.kt
@@ -1,15 +1,16 @@
 package com.jorcollmar.rickandmorty.domain
 
-import com.jorcollmar.rickandmorty.domain.mapper.toCharacter
 import com.jorcollmar.rickandmorty.data.remote.RickAndMortyApi
+import com.jorcollmar.rickandmorty.domain.mapper.toCharacter
 import com.jorcollmar.rickandmorty.domain.model.Character
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class CharacterRepository @Inject constructor(
     private val api: RickAndMortyApi
 ) {
-    fun getCharacterDetails(characterId: Int): Flow<Character> =
-        flow { api.getCharacter(characterId).toCharacter() }
+    suspend fun getCharacterDetails(characterId: Int): Result<Character> = try {
+        Result.success(api.getCharacter(characterId).toCharacter())
+    } catch (e: Exception) {
+        Result.failure(e)
+    }
 }
